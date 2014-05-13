27a469dbae81ef25a578b125e88db9bb2fd047b3
Roberto Carrillo <email@robertocarrillo.com>
2014-05-12 21:01:32 -0500

    Renamed to MaxSubarraySequence

--- MaxContigSequence/Program.cs
+++ MaxContigSequence/Program.cs
@@ -6,22 +6,10 @@ namespace MaxContigSequence
     {
         static void Main()
         {
-            //Console.WriteLine(MaxSequence(new[] {5, -2, 3}));
-            Console.WriteLine(MaxSequence(new[] {-2, -3, 4, -1, -2, 1, 5, -3}));
-        }
-
-        private static int MaxSequence(int[] a)
-        {
-            if (a == null) throw new ArgumentNullException("a");
-            int current = 0;
-            int max = 0;
-            for (int i = 0; i < a.Length; i++)
-            {
-                if (current < 0) current = 0;
-                current = current + a[i];
-                if (current > max) max = current;
-            }
-            return max;
+            var sequence = new[] {-2, -3, 4, -1, -2, 1, 5, -3};
+            var calculator = new SequenceCalculator();
+            int result = calculator.CalculateMaxSum(sequence);
+            Console.WriteLine(result);
         }
     }
 }
---
+++ MaxContigSequence/SequenceCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MaxContigSequence
+{
+    class SequenceCalculator
+    {
+        public int CalculateMaxSum(int[] sequence)
+        {
+            if (sequence == null) throw new ArgumentNullException("sequence");
+            int current = 0;
+            int max = 0;
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                if (current < 0) current = 0;
+                current = current + sequence[i];
+                if (current > max) max = current;
+            }
+            return max;
+        }
+    }
+}
